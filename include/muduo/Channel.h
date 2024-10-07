28a291a61d23d5b42b2a0eec96c1adaf4f6dc10d
yhj <814152205@qq.com>
2024-10-08 01:24:01 +0800

    Channel模块开发

--- include/muduo/Channel.h
+++ include/muduo/Channel.h
@@ -16,9 +16,42 @@ public:
 
     Channel(EventLoop *loop, int fd);
     ~Channel();
-
+    // fd得到poller通知以后,处理事件的
     void handleEvent(TimeStamp receiveTime);
+    // 设置回调函数对象
+    void setReadCallback(ReadEventCallback cb) { readCallback_ = std::move(cb); }
+    void setWriteCallback(EventCallback cb) { writeCallback_ = std::move(cb); }
+    void setCloseCallback(EventCallback cb) { closeCallback_ = std::move(cb); }
+    void setErrorCallback(EventCallback cb) { errorCallback_ = std::move(cb); }
+    // 防止channel被手动remove掉,channel还在执行回调操作,这块不太理解
+    void tie(const std::shared_ptr<void>&);
+
+    int fd() const { return fd_; }
+    int events() const { return events_; }
+    void setRevents(int revt) { revents_ = revt; }
+    bool isNoneEvent() const { return events_ == kNoneEvent; }
+
+    // 设置fd相应事件掉状态,update是操作poller中对fd的监听
+    void enableReadint() { events_ |= kReadEvent; update(); }
+    void disableReading() { events_ &= ~kReadEvent; update(); }
+    void enableWriting() { events_ |= kWriteEvent; update(); }
+    void disableWriting() { events_ &= ~kWriteEvent; update(); }
+    void disableAll() { events_ = kNoneEvent; update(); }
+
+    // 检查fd当前在poller注册的感兴趣的事件
+    bool isNoneEvent() { return events_ == kNoneEvent; }
+    bool isWriting() { return events_ == kWriteEvent; }
+    bool isReading() { return events_ == kReadEvent; }
+
+    int index() { return index_; }
+    void setIndex(int idx) { index_ = idx; }
+
+    // one loop per thread
+    EventLoop* ownerLoop() { return loop_; }
+    void remove();
 private:
+    void update();
+    void handleEventWithGuard(TimeStamp receiveTime);
     static const int kNoneEvent;
     static const int kReadEvent;
     static const int kWriteEvent;
--- src/muduo/Channel.cpp
+++ src/muduo/Channel.cpp
@@ -1,3 +1,78 @@
-//
-// Created by 请给我来盆饭 on 2024/10/7.
-//
+#include "Channel.h"
+#include "TimeStamp.h"
+#include "Logger.h"
+#include <sys/epoll.h>
+
+const int Channel::kNoneEvent = 0;
+const int Channel::kReadEvent = EPOLLIN | EPOLLPRI;
+const int Channel::kWriteEvent = EPOLLOUT;
+
+Channel::Channel(EventLoop *loop, int fd)
+    : loop_(loop),
+      fd_(fd),
+      events_(0),
+      revents_(0),
+      index_(-1),
+      tied_(false)
+{
+
+}
+
+Channel::~Channel()
+{
+
+}
+// 该方法何时被调用
+void Channel::tie(const std::shared_ptr<void> &obj)
+{
+    tie_ = obj;
+    tied_ = true; // 弱指针已绑定强指针
+}
+
+// 改变fd感兴趣的事件后,在poller中调用epoll_ctl
+void Channel::update()
+{
+    // 通过Channel所属的EventLoop,调用poller的相关方法,修改epoll上注册的事件
+    // loop_->updateChannel(this);
+}
+// 在Channel所属的EventLoop中删除当前的Channel
+void Channel::remove()
+{
+    // loop_->removeChannel(this);
+}
+// fd得到Poller通知以后,处理事件
+void Channel::handleEvent(TimeStamp receiveTime)
+{
+    std::shared_ptr<void> guard;
+    if (tied_) {
+        guard = tie_.lock();
+        if (guard) {
+            handleEventWithGuard(receiveTime);
+        }
+    }
+    else {
+        handleEventWithGuard(receiveTime);
+    }
+}
+// 根据poller通知的Channel发生的具体事件,执行对应的回调
+void Channel::handleEventWithGuard(TimeStamp receiveTime)
+{
+    LOG_INFO("channel handleEvent revents:%d\n", revents_);
+    if ((revents_ & EPOLLHUP) && !(revents_ & EPOLLIN)) {
+        if (closeCallback_) {
+            closeCallback_();
+        }
+    }
+
+    if (revents_ & (EPOLLIN | EPOLLPRI)) {
+        if (readCallback_) {
+            readCallback_(receiveTime);
+        }
+    }
+
+    if (revents_ & EPOLLOUT) {
+        if (writeCallback_) {
+            writeCallback_();
+        }
+    }
+}
